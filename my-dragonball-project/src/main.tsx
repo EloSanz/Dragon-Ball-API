cd728935e88dfaf17b8e40dce3bb54b0662bb0cf
ElitoSanz <sanzeliseo03@gmail.com>
2024-10-15 12:21:33 -0300

    test

--- my-dragonball-project/src/main.tsx
+++ my-dragonball-project/src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { FiltersProvider } from './FiltersProvider.tsx'
-
+// testing github actions
 createRoot(document.getElementById('root')!).render(
   <FiltersProvider>
     <App />
